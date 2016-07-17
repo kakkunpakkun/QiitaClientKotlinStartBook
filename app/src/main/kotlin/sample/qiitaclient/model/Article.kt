029d18417e35dd8e2c3ba52aae1260fa2478a795
kakkunpakkun <kakkunpakkun@gmail.com>
2016-07-17 11:18:49 +0900

    add models

---
+++ app/src/main/kotlin/sample/qiitaclient/model/Article.kt
@@ -0,0 +1,10 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by kakkunpakkun on 2016/07/17.
+ */
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: Stirng,
+                   val user: User)
---
+++ app/src/main/kotlin/sample/qiitaclient/model/User.kt
@@ -0,0 +1,9 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by kakkunpakkun on 2016/07/17.
+ */
+
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
