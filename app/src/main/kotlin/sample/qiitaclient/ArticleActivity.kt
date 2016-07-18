31a52aac65fbde206cd7b1eaa7f2be9b12def342
kakkunpakkun <kakkunpakkun@gmail.com>
2016-07-18 15:36:30 +0900

    article view with webview

---
+++ app/src/main/kotlin/sample/qiitaclient/ArticleActivity.kt
@@ -0,0 +1,35 @@
+package sample.qiitaclient
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
+import sample.qiitaclient.model.Article
+import sample.qiitaclient.view.ArticleView
+
+/**
+ * Created by kakkunpakkun on 2016/07/17.
+ */
+
+class ArticleActivity : AppCompatActivity() {
+    companion object {
+        private const val ARTICLE_EXTRA: String = "article"
+
+        fun intent(context: Context, article: Article): Intent =
+                Intent(context, ArticleActivity::class.java)
+                    .putExtra(ARTICLE_EXTRA, article)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+        val articleView = findViewById(R.id.article_view) as ArticleView
+        val webView = findViewById(R.id.web_view) as WebView
+
+        val article: Article = intent.getParcelableExtra(ARTICLE_EXTRA)
+        articleView.setArticle(article)
+        webView.loadUrl(article.url)
+    }
+}
--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -2,6 +2,7 @@ package sample.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
 import sample.qiitaclient.view.ArticleView
@@ -10,14 +11,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(
+                dummyArticle("Kotlin入門", "太郎"),
+                dummyArticle("Java入門", "次郎")
+        )
 
-        articleView.setArticle(Article(id = "123",
-                title = "Kotlin入門",
-                url = "http://www.example.com/articles/123",
-                user = User(id = "456", name = "たろう", profileImageUrl = "")))
-
-        setContentView(articleView)
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            val article = listAdapter.articles[position]
+            ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
